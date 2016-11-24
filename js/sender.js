9e6dabb82ea7f4ed04d72857157412f21b121dc1
AnDo <KeeperAnDo@yandex.ru>
2016-11-24 12:00:03 +0200

    email form

--- js/sender.js
+++ js/sender.js
@@ -3,8 +3,8 @@ $(function() {
 //-----------------------------------VALIDATION-------------------------------------
 //==================================================================================
 	var expEmail = /^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-	expLettersOnly = /^[a-zA-Z\s]+$/,
-	expPhone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+	expLettersOnly = /^[а-яА-ЯёЁa-zA-Z\s]+$/,
+	expPhone = /^([+]?[0-9\s-\(\)]{9,25})*$/;
 
 
 	$('#mail-form').on( 'keyup', 'input', function() {
@@ -58,31 +58,24 @@ $(function() {
 	};
 
 	function success() {
-		// function complete(){
-		// 	$(".email-success").fadeIn();
-		// };
 
-		// setTimeout(function(){ 
-		// 	$(".email-message").fadeOut(500, complete);
-		// 	$(".email-block").fadeOut(500, complete);
-		// }, 1500);
-	}
-
-	function error(response) {
-		alert(response)
+		function complete(){
+			$(".loader").fadeOut();
+			$(".success").fadeIn();
+		};
+		$(".loader").fadeIn();
+		setTimeout(function(){ complete();}, 1500);
 	}
 
 	function sendEmail(email,phone,name,comment) {
 		$.ajax({
-			url: "https://vah.github.io/bart/js/sender.php",
+			url: "http://pastela.com.ua/bart/js/sender.php",
 			type : "POST",
 			cache : false,
 			data : { email: email , phone: phone, name: name, comment: comment},
 			success: function(response){
 				if(response.indexOf("TRUE") !== -1){
 					success();
-				} else {
-					error(response);
 				}
 			}
 		});
